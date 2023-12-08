32dbadfd2d1d2f92e414e46b8fa8236714d8931a
Gabe Payumo <gabe.payumo@mail.utoronto.ca>
2023-12-07 21:32:52 -0500

    changed gif and message for corrina

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ yesBtn.addEventListener("click", () => {
     "https://media4.giphy.com/media/F9Z5dbg6Rkn7nf6mAb/giphy.gif?cid=2154d3d70dghsxg4ey8jz5snkyvhmx2gvbmpa8y8vlsvrji8&ep=v1_gifs_related&rid=giphy.gif&ct=g";
 });
 
-noBtn.addEventListener("mouseover", () => {
+noBtn.addEventListener("click", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
